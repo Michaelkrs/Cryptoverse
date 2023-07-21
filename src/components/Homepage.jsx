09f9769a2bd02fd080ae28c82bed868b4c183581
Michaelkrs <kristianmichael10@gmail.com>
2023-07-21 15:58:31 +0700

    Feat: News

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,16 +3,17 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from "../components";
 const { Title } = Typography; // to make <Typography.Title> become just <Title>
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10); // show only 10 data
   const globalStats = data?.data?.stats;
 
   if (isFetching) return "Loading...";
 
   return (
-    <>
+    <div style={{ margin: "20px" }}>
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
@@ -48,7 +49,28 @@ const Homepage = () => {
           ></Statistic>
         </Col>
       </Row>
-    </>
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      {/* simplified is true so it becomes prop, to show only 10 data */}
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
+    </div>
   );
 };
 
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "50287a454cmshf8577d41c42a96cp19f5bfjsn362375b487bf",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders }); // to make it easier, just pass in url
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
